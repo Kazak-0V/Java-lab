b9220d9e4733f4c5829e52090158dbe7c48c32cd
Kazak-0V <mr.kin.10@mail.ru>
2025-11-03 11:02:48 +0500

    Update Main.java

--- Java Lab 3/src/ru/Kazakov/task1a/Main.java
+++ Java Lab 3/src/ru/Kazakov/task1a/Main.java
@@ -13,24 +13,25 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // 1. Хранилище чисел с null (альтернатива 0)
+        // Хранилище чисел с null (альтернатива 0)
         Storage<Integer> numberStorage1 = new Storage<>(null, 0);
         System.out.println(numberStorage1);
         processNumberStorage(numberStorage1);
 
-        // 2. Хранилище чисел с 99 (альтернатива -1)
+        // Хранилище чисел с 99 (альтернатива -1)
         Storage<Integer> numberStorage2 = new Storage<>(99, -1);
         System.out.println(numberStorage2);
         processNumberStorage(numberStorage2);
 
-        // 3. Хранилище строк с null (альтернатива "default")
+        // Хранилище строк с null (альтернатива "default")
         Storage<String> stringStorage1 = new Storage<>(null, "default");
         System.out.println(stringStorage1);
         processStringStorage(stringStorage1);
 
-        // 4. Хранилище строк с "hello" (альтернатива "hello world")
+        // Хранилище строк с "hello" (альтернатива "hello world")
         Storage<String> stringStorage2 = new Storage<>("hello", "hello world");
         System.out.println(stringStorage2);
         processStringStorage(stringStorage2);
     }
+
 }
