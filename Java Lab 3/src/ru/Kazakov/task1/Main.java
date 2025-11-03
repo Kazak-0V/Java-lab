d716452c48ae1431232284f04063e10ff0ed8d79
Kazak-0V <mr.kin.10@mail.ru>
2025-11-03 10:59:35 +0500

    Update Main.java

--- Java Lab 3/src/ru/Kazakov/task1/Main.java
+++ Java Lab 3/src/ru/Kazakov/task1/Main.java
@@ -10,14 +10,12 @@ public class Main {
     public static void main(String[] args) {
         Box<Integer> box = new Box<>();
 
-        // Размещаем число 3 как сказано в задании
         box.put(3);
         System.out.println(box);
 
-        // Передаем коробку в метод и извлекаем значение
         showValueFromBox(box);
 
-        // Показываем что коробка пуста
         System.out.println("После извлечения: " + box);
     }
+
 }
